0a0a15113cda8c8d80693c32f7a71e21ed0436bc
Ivan <103671783+tarenkovia@users.noreply.github.com>
2023-04-17 18:29:24 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,13 @@ public class Main {
         Transformer transformer1 = manipulator.creatureTransformers("Бамбл Би", "Ярко-желтый",
                 "Лазерная пушка", "Отбойный молот");
         System.out.println(transformer1);
+        
+        System.out.println();
+
+        TransformerBuilder transformerFrac2 = new DecepticonsTransformerBuilder();
+        FirstCybertronFactory manipulator2 = new FirstCybertronFactory(transformerFrac2);
+        Transformer transformer2 = manipulator2.creatureTransformers("Стар Скрим", "Темно-серый",
+                "Пулемет", "Разбойничий нож");
+        System.out.println(transformer2);
     }
 }
