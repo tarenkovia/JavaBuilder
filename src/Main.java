b7ecee00cba3c5d71addb5454e72bc0f175b51d8
tarenkovia <tarenkov_ivan_00@mail.ru>
2023-04-17 18:28:43 +0400

    Добавление нового трансфомера

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        TransformerBuilder transformer = new AutobotsTransformerBuilder();
-        FirstCybertronFactory manipulator = new FirstCybertronFactory(transformer);
-        Transformer transformer1 = manipulator.creatureTransformers("Бамбл Би", "Ярко-желтый",
+        TransformerBuilder transformerFrac1 = new AutobotsTransformerBuilder();
+        FirstCybertronFactory manipulator1 = new FirstCybertronFactory(transformerFrac1);
+        Transformer transformer1 = manipulator1.creatureTransformers("Бамбл Би", "Ярко-желтый",
                 "Лазерная пушка", "Отбойный молот");
         System.out.println(transformer1);
+
+        System.out.println();
+
+        TransformerBuilder transformerFrac2 = new DecepticonsTransformerBuilder();
+        FirstCybertronFactory manipulator2 = new FirstCybertronFactory(transformerFrac2);
+        Transformer transformer2 = manipulator2.creatureTransformers("Стар Скрим", "Темно-серый",
+                "Пулемет", "Разбойничий нож");
+        System.out.println(transformer2);
     }
 }
